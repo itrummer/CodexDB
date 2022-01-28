f39aed0882afe2d917c2a31b8e34f443fae2aae4
itrummer <immanuel.trummer@gmail.com>
2022-01-28 15:36:49 -0500

    Added check whether benchmark output directory is empty

--- src/codexdb/benchmark.py
+++ src/codexdb/benchmark.py
@@ -6,17 +6,20 @@ Created on Oct 3, 2021
 import argparse
 import codexdb.solve
 import openai
+import os
 
 
-def train(data_dir, train_path, log_path, result_path, 
-          mod_start='Import pandas library', 
-          mod_between='', mod_end=''):
+def train(data_dir, train_path, log_path, mod_start,
+          mod_between, mod_end, result_path):
     """ Generate training samples for few-shot learning.
     
     Args:
         data_dir: path to database directory
         train_path: path to test cases for training
         log_path: path to file for logging output
+        mod_start: modification at plan start
+        mod_between: modification between plan steps
+        mod_end: modification at plan end
         result_path: path to file for result output
     """
     codexdb.solve.main(
@@ -24,13 +27,18 @@ def train(data_dir, train_path, log_path, result_path,
         mod_start, mod_between, mod_end, '', 0, 50, 'solved', 10, 
         log_path, result_path)
 
-def test_plain(data_dir, test_path, sample_path, out_dir):
+def test_plain(
+        data_dir, test_path, sample_path, mod_start, 
+        mod_between, mod_end, out_dir):
     """ Solve test cases without any user-defined modifications.
     
     Args:
         data_dir: directory of database
         test_path: path to file with test cases
         sample_path: path to file with samples
+        mod_start: modification at plan start
+        mod_between: modification between plan steps
+        mod_end: modification at plan end
         out_dir: generate output in this directory
     """
     for model_id in ['cushman-codex', 'davinci-codex']:
@@ -41,7 +49,7 @@ def test_plain(data_dir, test_path, sample_path, out_dir):
                 result_path = f'{out_dir}/results_{run_id}.json'
                 codexdb.solve.main(
                     data_dir, test_path, 'python', model_id, 
-                    prompt_style, 'Import pandas library', '', '', 
+                    prompt_style, mod_start, mod_between, mod_end, 
                     sample_path, nr_samples, 100, 'executed', 1, 
                     log_path, result_path)
 
@@ -53,13 +61,21 @@ if __name__ == '__main__':
     parser.add_argument('data_dir', type=str, help='Data directory')
     parser.add_argument('train_path', type=str, help='Path to train case file')
     parser.add_argument('test_path', type=str, help='Path to test case file')
+    parser.add_argument('mod_start', type=str, help='Modifying plan start')
+    parser.add_argument('mod_between', type=str, help='Modifications in between')
+    parser.add_argument('mod_end', type=str, help='Modifying plan end')
     parser.add_argument('out_dir', type=str, help='Output directory')
     args = parser.parse_args()
+    if os.listdir(args.out_dir):
+        raise ValueError('Output directory must be empty!')
     
     openai.api_key = args.ai_key
     
     # Train and test generating code without modifications
     log_path = f'{args.out_dir}/train_log_plain'
     sample_path = f'{args.out_dir}/train_plain.json'
-    train(args.data_dir, args.train_path, log_path, sample_path)
-    test_plain(args.data_dir, args.test_path, sample_path, args.out_dir)
+    train(args.data_dir, args.train_path, log_path, args.mod_start, 
+          args.mod_between, args.mod_end, sample_path)
+    test_plain(args.data_dir, args.test_path, sample_path, 
+               args.mod_start, args.mod_between, args.mod_end, 
+               args.out_dir)
