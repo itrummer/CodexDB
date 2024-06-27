ab5505a88e8b89d4fb7f882a862a7477dc089ae0
itrummer <immanuel.trummer@gmail.com>
2024-06-27 11:11:39 -0400

    Check for more types of print statements in code analysis.

--- src/codexdb/bench/plot.py
+++ src/codexdb/bench/plot.py
@@ -91,7 +91,8 @@ def analyze_code(run_dir):
     
     reg_count = collections.defaultdict(lambda:0)
     reg_exps = [
-        'print\(\'Done\.\'\)', 'print\(([a-zA-Z_])+\)', 
+        'print\(\'Done\.\'\)', 'print\(\'Done\'\)',
+        'print\(([a-zA-Z_])+\)',
         'print\(["|\'].+["|\']\)', 'print']
     for tries in tries_by_case:
         code = tries[-1]['code']
